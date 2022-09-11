467fa858da6849d278e85821135c5358acc90b25
Denisio Rordrigues <denisio@ymail.com>
2022-09-11 08:59:20 -0300

    Configuração Swagger e ações de cadastrar e entrar

--- src/services/Ecomerce.Identidade.API/Startup.cs
+++ src/services/Ecomerce.Identidade.API/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Ecomerce.Identidade.API
 {
@@ -37,11 +38,28 @@ namespace Ecomerce.Identidade.API
                 .AddDefaultTokenProviders();
             
             services.AddControllers();
+
+            services.AddSwaggerGen(c => 
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Ecomerce Identidade API",
+                    Description = "Está API faz parte dos estudos e para compor meu portifólio",
+                    Contact = new OpenApiContact() { Name = "Denisio Rodrigues", Email = "denisio@ymail.com" },
+                    License = new OpenApiLicense() { Name = "MIT", Url= new Uri("https://opensource.org/licenses/MIT") }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint(url: "/swagger/v1/swagger.json", "v1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
