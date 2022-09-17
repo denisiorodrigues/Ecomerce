9d072f1b7e572d98147fa50d9f36526f6e61dbfa
Denisio Rordrigues <denisio@ymail.com>
2022-09-17 14:31:08 -0300

    Refatorando a startup e colocando as configuração em cada arquivo

--- src/services/Ecomerce.Identidade.API/Startup.cs
+++ src/services/Ecomerce.Identidade.API/Startup.cs
@@ -1,3 +1,4 @@
+using Ecomerce.Identidade.API.Configurations;
 using Ecomerce.Identidade.API.Data;
 using Ecomerce.Identidade.API.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -38,82 +39,19 @@ namespace Ecomerce.Identidade.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string connectionString = Configuration.GetConnectionString("DefaultConnection");
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddIdentityConfig(Configuration);
 
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddRoles<IdentityRole>()
-                .AddErrorDescriber<IdentityTranslatePtBr>() // Tradução
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+            services.AddApiConfig();
 
-            var appSettingSection = Configuration.GetSection("AppSettings");
-            services.Configure<AppSettings>(appSettingSection);
-            
-            var appSettings = appSettingSection.Get<AppSettings>();
-            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-
-            //JTW
-            //Configurando o modelo de nautenticação para usar o JWT
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(bearerOptions =>
-            {
-                bearerOptions.RequireHttpsMetadata = true;//somente acesso ao http
-                bearerOptions.SaveToken = true;//Salvar o token na instância assim que o login for realizado comc uscesso
-                bearerOptions.TokenValidationParameters = new TokenValidationParameters //Configuyração do token
-                {
-                    ValidateIssuerSigningKey = true,//Validar o emissor com base na assinatura
-                    IssuerSigningKey = new SymmetricSecurityKey(key),//Criando o emissor
-                    ValidateIssuer = true, // Validar sendo da API que foi configurada
-                    ValidateAudience = true, // Onde esse token vai ser válido(em que site)
-                    ValidAudience = appSettings.ValidadoEm, //Criando a audiencia
-                    ValidIssuer = appSettings.Emissor //Criando um Issuer
-                };
-            });
-            //Fim da documentação
-
-            services.AddControllers();
-
-            services.AddSwaggerGen(c => 
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "Ecomerce Identidade API",
-                    Description = "Está API faz parte dos estudos e para compor meu portifólio",
-                    Contact = new OpenApiContact() { Name = "Denisio Rodrigues", Email = "denisio@ymail.com" },
-                    License = new OpenApiLicense() { Name = "MIT", Url= new Uri("https://opensource.org/licenses/MIT") }
-                });
-            });
+            services.AddSwaggerConfig();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint(url: "/swagger/v1/swagger.json", "v1");
-            });
+            app.UseSwaggerConfig();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-            app.UseAuthentication();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseApiConfig(env);
         }
     }
 }
