0249d576edf2bcda66e4cce4532d0da1718af5a6
Denisio Rordrigues <denisio@ymail.com>
2022-09-17 14:31:08 -0300

    Refatorando a startup e colocando as configuração em cada arquivo

---
+++ src/services/Ecomerce.Identidade.API/Configurations/IdentityConfig.cs
@@ -0,0 +1,67 @@
+﻿using Ecomerce.Identidade.API.Data;
+using Ecomerce.Identidade.API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Ecomerce.Identidade.API.Configurations
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddIdentityConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddErrorDescriber<IdentityTranslatePtBr>() // Tradução
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            var appSettingSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingSection);
+
+            var appSettings = appSettingSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            //JTW
+            //Configurando o modelo de nautenticação para usar o JWT
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(bearerOptions =>
+            {
+                bearerOptions.RequireHttpsMetadata = true;//somente acesso ao http
+                bearerOptions.SaveToken = true;//Salvar o token na instância assim que o login for realizado comc uscesso
+                bearerOptions.TokenValidationParameters = new TokenValidationParameters //Configuyração do token
+                {
+                    ValidateIssuerSigningKey = true,//Validar o emissor com base na assinatura
+                    IssuerSigningKey = new SymmetricSecurityKey(key),//Criando o emissor
+                    ValidateIssuer = true, // Validar sendo da API que foi configurada
+                    ValidateAudience = true, // Onde esse token vai ser válido(em que site)
+                    ValidAudience = appSettings.ValidadoEm, //Criando a audiencia
+                    ValidIssuer = appSettings.Emissor //Criando um Issuer
+                };
+            });
+            //Fim da documentação
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseIdentityConfig(this IApplicationBuilder app)
+        {
+            app.UseAuthorization();
+            app.UseAuthentication();
+
+            return app;
+        }
+    }
+}
---
+++ src/services/Ecomerce.Identidade.API/Configurations/SwaggerConfig.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace Ecomerce.Identidade.API.Configurations
+{
+    public static class SwaggerConfig
+    {
+        public static IServiceCollection AddSwaggerConfig(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "Ecomerce Identidade API",
+                    Description = "Está API faz parte dos estudos e para compor meu portifólio",
+                    Contact = new OpenApiContact() { Name = "Denisio Rodrigues", Email = "denisio@ymail.com" },
+                    License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
+                });
+            });
+
+            return services;
+        }
+
+
+        public static IApplicationBuilder UseSwaggerConfig(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint(url: "/swagger/v1/swagger.json", "v1");
+            });
+
+            return app;
+        }
+    }
+}
