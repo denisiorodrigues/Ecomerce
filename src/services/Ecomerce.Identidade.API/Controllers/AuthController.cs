4bfa942003d8f0c1f9df88d5a4d885b01d70fe78
Denisio Rordrigues <denisio@ymail.com>
2022-09-17 13:53:58 -0300

    Refatorando a Auth controller

--- src/services/Ecomerce.Identidade.API/Controllers/AuthController.cs
+++ src/services/Ecomerce.Identidade.API/Controllers/AuthController.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
@@ -84,6 +86,33 @@ namespace Ecomerce.Identidade.API.Controllers
         {
             var user = await _userManager.FindByEmailAsync(email);
             var claims = await _userManager.GetClaimsAsync(user);
+
+            var identityClaims = await ObterClaimsUsuario(claims, user);
+
+            var encodedToken = CodificarToken(identityClaims);
+
+            return ObterRespostaToken(encodedToken, user, claims);
+        }
+
+        private string CodificarToken(ClaimsIdentity identityClaims)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSetting.Secret);
+
+            var token = tokenHandler.CreateToken(new Microsoft.IdentityModel.Tokens.SecurityTokenDescriptor
+            {
+                Issuer = _appSetting.Emissor,
+                Audience = _appSetting.ValidadoEm,
+                Subject = identityClaims,
+                Expires = DateTime.UtcNow.AddHours(_appSetting.ExpiracaoHoras),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+            });
+
+           return tokenHandler.WriteToken(token);
+        }
+
+        private async Task<ClaimsIdentity> ObterClaimsUsuario(ICollection<Claim> claims, IdentityUser user)
+        {
             var userRoles = await _userManager.GetRolesAsync(user);
 
             claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
@@ -101,32 +130,23 @@ namespace Ecomerce.Identidade.API.Controllers
             var identityClaims = new ClaimsIdentity();
             identityClaims.AddClaims(claims);
 
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(_appSetting.Secret);
+            return identityClaims;
+        }
 
-            var token = tokenHandler.CreateToken(new Microsoft.IdentityModel.Tokens.SecurityTokenDescriptor
-            {
-                Issuer = _appSetting.Emissor,
-                Audience = _appSetting.ValidadoEm,
-                Subject = identityClaims,
-                Expires = DateTime.UtcNow.AddHours(_appSetting.ExpiracaoHoras),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
-            });
-
-            var encodedToken = tokenHandler.WriteToken(token);
-
+        private UsuarioRespostaLogin ObterRespostaToken(string encodedToken, IdentityUser user, IEnumerable<Claim> claims)
+        {
             var response = new UsuarioRespostaLogin()
-            { 
+            {
                 AccessToken = encodedToken,
                 ExpiresIn = TimeSpan.FromHours(_appSetting.ExpiracaoHoras).TotalSeconds,
-                UsuarioToken = new UsuarioToken() 
+                UsuarioToken = new UsuarioToken()
                 {
                     Id = user.Id,
                     Email = user.Email,
                     Claims = claims.Select(x => new UsuarioClaim() { Type = x.Type, Value = x.Value })
-                } 
+                }
             };
-            
+
             return response;
         }
 
